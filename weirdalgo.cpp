8431e2de9f4f73719180beee64e7f5e88c06f988
Meghana Agnur <meghanaagnur@gmail.com>
2026-08-17 06:30:05 +0530

    Weird algorithm CSES

---
+++ weirdalgo.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long int n;
+    std::cin>>n;
+    std::cout<<n<<" ";
+	while(n>1){
+    if(n%2==0){
+        n=n/2l;
+    }
+    else{
+        n=(3*n)+1;
+    }
+    std::cout<<n<<" ";
+	}
+}
